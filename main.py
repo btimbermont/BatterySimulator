94a6b3fc092f8accc951b17418690481b73631e2
Bouke Timbermont <32333531+btimbermont@users.noreply.github.com>
2025-03-15 13:45:03 +0100

    Enable simulating multiple batteries in one run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dataclasses import dataclass
-from typing import List
+from typing import List, Dict
 
 from battery_sim import Battery, ws_to_kwh, home_wizard_battery
 
@@ -8,7 +8,7 @@ from battery_sim import Battery, ws_to_kwh, home_wizard_battery
 @dataclass
 class PowerDatapoint:
     time: int
-    usage: int
+    consumption: int
     production: int
 
 
@@ -30,46 +30,57 @@ def get_first_timestamp(path: str) -> int:
             return int(values[0])
 
 
-def simulate_battery(battery: Battery, mbc_files: List[str]):
+def simulate_battery(batteries: Dict[str, Battery], mbc_files: List[str]):
     previous_data = None
     consumption, production = 0, 0
-    consumption_with_battery, production_with_battery = 0, 0
+    battery_results = {name: [0, 0] for name, _ in batteries.items()}
     for mbc_file in sorted(mbc_files, key=get_first_timestamp):
         for current_datapoint in parse_mbc_file(mbc_file):
             if previous_data is not None:
                 duration = current_datapoint.time - previous_data.time
                 if duration == 0:
+                    # consecutive files have 1 overlapping data point, we can ignore this datapoint
                     ...
-                elif current_datapoint.production > 0 and current_datapoint.usage > 0:
+                elif current_datapoint.production > 0 and current_datapoint.consumption > 0:
+                    # TODO: figure out what to do with this
                     # print(f'consumption and production at the same time is not supported!')
                     ...
                 elif current_datapoint.production > 0:
                     # baseline
                     production += current_datapoint.production * duration
-                    # with battery
-                    share_used = battery.charge(current_datapoint.production, duration)
-                    production_with_battery += (1 - share_used) * current_datapoint.production * duration
-                elif current_datapoint.usage > 0:
+                    # with batteries
+                    for name, battery in batteries.items():
+                        if battery._available_capacity == 0:  # shortcut saves some time: full battery can't charge
+                            battery_results[name][1] += current_datapoint.production * duration
+                        else:
+                            share_used = battery.charge(current_datapoint.production, duration)
+                            battery_results[name][1] += (1 - share_used) * current_datapoint.production * duration
+                elif current_datapoint.consumption > 0:
                     # baseline
-                    consumption += current_datapoint.usage * duration
-                    # with battery
-                    share_done_with_battery = battery.discharge(current_datapoint.usage, duration)
-                    consumption_with_battery += (1 - share_done_with_battery) * current_datapoint.usage * duration
+                    consumption += current_datapoint.consumption * duration
+                    # with batteries
+                    for name, battery in batteries.items():
+                        if battery._current_capacity == 0:  # shortcut saves some time: empty battery can't deliver power
+                            battery_results[name][0] += current_datapoint.consumption * duration
+                        else:
+                            share_done_with_battery = battery.discharge(current_datapoint.consumption, duration)
+                            battery_results[name][0] += (1 - share_done_with_battery) * current_datapoint.consumption * duration
             previous_data = current_datapoint
-    consumption = ws_to_kwh(consumption)
-    production = ws_to_kwh(production)
-    consumption_with_battery = ws_to_kwh(consumption_with_battery)
-    production_with_battery = ws_to_kwh(production_with_battery)
     print(f'Simulation done!')
-    print(f'without battery: consumed {consumption:.2f} kWh, produced {production:.2f}kWh')
-    print(f'with battery: consumed {consumption_with_battery:.2f} kWh, produced {production_with_battery:.2f}kWh')
+    print(f'without battery: consumed {ws_to_kwh(consumption):.2f} kWh, produced {ws_to_kwh(production):.2f}kWh')
+    print('With batteries:')
+    for name, (consumption, production) in battery_results.items():
+        print(f'{name}: consumed {ws_to_kwh(consumption):.2f} kWh, produced {ws_to_kwh(production):.2f}kWh')
 
 
 if __name__ == '__main__':
     mbc_files = [f'input/{f}' for f in os.listdir('input')]
-    battery = home_wizard_battery(1)
-    simulate_battery(battery, mbc_files)
-    battery = home_wizard_battery(2, True)
-    simulate_battery(battery, mbc_files)
-    battery = home_wizard_battery(3, True)
-    simulate_battery(battery, mbc_files)
+    batteries = {
+        'Saldering': Battery(100000, 100000, 100000, 1.0),
+        'HomeWizard x 1': home_wizard_battery(),
+        'HomeWizard x 2': home_wizard_battery(2),
+        'HomeWizard x 2 (eigen groep)': home_wizard_battery(2, True),
+        'HomeWizard x 3': home_wizard_battery(3),
+        'HomeWizard x 3 (eigen groep)': home_wizard_battery(3, True)
+    }
+    simulate_battery(batteries, mbc_files)
